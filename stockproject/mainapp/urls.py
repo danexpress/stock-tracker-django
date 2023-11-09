bce9a8ffa6682952527ac9ad5af01b856c2bfa39
Fred Offei <112915741+danexpress@users.noreply.github.com>
2023-11-09 18:43:09 -0500

    yahoo-fin integration

--- stockproject/mainapp/urls.py
+++ stockproject/mainapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.stockPicker, name="stockpicker"),
+    path("stocktracker/", views.stockTracker, name="stocktracker"),
 ]
--- stockproject/mainapp/views.py
+++ stockproject/mainapp/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from yahoo_fin.stock_info import *
 
 
 # Create your views here.
 def stockPicker(request):
-    return render(request, "mainapp/stockpicker.html")
+    stock_picker = tickers_nifty50()
+    print(stock_picker)
+    return render(request, "mainapp/stockpicker.html", {"stockpicker": stock_picker})
+
+
+def stockTracker(request):
+    stockpicker = request.GET.getlist("stockpicker")
+    print(stockpicker)
+    data = {}
+    available_stocks = tickers_nifty50()
+    for i in stockpicker:
+        if i in available_stocks:
+            pass
+        else:
+            return HttpResponse("Error")
+    for i in stockpicker:
+        result = get_quote_table("aapl")
+        # data.update({i: result})
+
+    print(result)
+
+    return render(request, "mainapp/stocktracker.html")
